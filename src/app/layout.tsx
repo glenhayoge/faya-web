c1a8583423742acef71b4a2e1ba590c316383150
Glen Hayoge <glenhayoge@gmail.com>
2024-02-12 07:36:46 +1000

    updated readme file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
-import './style.css';
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '@/prismicio';
+import './styles.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -16,9 +17,12 @@ export default function RootLayout({
           href="https://prismic.io/favicon.ico"
         />
       </head>
-      <body>
-        {children}
-        <PrismicPreview repositoryName={repositoryName} />
+
+      <body className="flex flex-col items-center bg-stone-50">
+        <div className="bg-white max-w-7xl min-h-screen border-x border-solid border-gray-200 p-12 w-full flex flex-col gap-20 items-center text-slate-700">
+          {children}
+          <PrismicPreview repositoryName={repositoryName} />
+        </div>
       </body>
     </html>
   );
