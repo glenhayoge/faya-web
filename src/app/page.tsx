60523dbb5446c90ee75834386d520f298c31dc84
Glen Hayoge <glenhayoge@gmail.com>
2024-02-12 07:53:09 +1000

    updated home page, created uid for blogs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+// ./src/app/page.tsx
+
 import { Metadata } from "next";
 
 import { SliceZone } from "@prismicio/react";
@@ -5,6 +7,8 @@ import * as prismic from "@prismicio/client";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
+import { PostCard } from "@/components/PostCard";
+import { Navigation } from "@/components/Navigation";
 
 /**
  * This component renders your homepage.
@@ -33,11 +37,34 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function Index() {
-  /**
-   * The client queries content from the Prismic API
-   */
+  // The client queries content from the Prismic API
   const client = createClient();
+
+  // Fetch the content of the home page from Prismic
   const home = await client.getByUID("page", "home");
 
-  return <SliceZone slices={home.data.slices} components={components} />;
+  // Get all of the blog_post documents created on Prismic ordered by publication date
+  const posts = await client.getAllByType("blog_post", {
+    orderings: [
+      { field: "my.blog_post.publication_date", direction: "desc" },
+      { field: "document.first_publication_date", direction: "desc" },
+    ],
+  });
+
+  return (
+    <>
+      <Navigation client={client} />
+
+      <SliceZone slices={home.data.slices} components={components} />
+
+      {/* Map over each of the blog posts created and display a `PostCard` for it */}
+      <section className="grid grid-cols-1 gap-8 max-w-3xl w-full">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </section>
+
+      <Navigation client={client} />
+    </>
+  );
 }
